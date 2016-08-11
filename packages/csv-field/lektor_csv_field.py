1e902418fa9bba141e29bf35474fb74818da604a
Paul McLanahan <pmac@mozilla.com>
2016-08-10 23:12:14 -0400

    Add csv-field plugin and convert for-players data

---
+++ packages/csv-field/lektor_csv_field.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import csv
+from cStringIO import StringIO
+
+from lektor.pluginsystem import Plugin
+from lektor.types import Type
+
+
+def csv_to_dicts(text):
+    text = text.strip()
+    if text:
+        return csv.DictReader(StringIO(text), skipinitialspace=True)
+
+
+class CsvType(Type):
+    widget = 'multiline-text'
+
+    def value_from_raw(self, raw):
+        return csv_to_dicts(raw.value or u'')
+
+
+class CsvFieldPlugin(Plugin):
+    name = u'csv-field'
+    description = u'Add a CSV field type for models.'
+
+    def on_setup_env(self):
+        self.env.add_type(CsvType)
