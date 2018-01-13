f811bf300a8e41d08cba0375d6afcca357889afc
Paul McLanahan <pmac@mozilla.com>
2018-01-12 21:15:39 -0500

    Move deployment to Netlify

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:latest
-
-COPY nginx.conf /etc/nginx/nginx.conf
-COPY build /usr/share/nginx/html
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,4 @@
 #!/bin/bash -ex
 
-lektor build -O _publish/build
-cp Dockerfile nginx.conf _publish/
-cd _publish
-git init
-git remote add dokku dokku@$DOKKU_HOST:$DOKKU_APP_NAME
-git add .
-git commit -m "add built site"
-git push -f dokku master
+lektor build -O public
+cp _headers public/
